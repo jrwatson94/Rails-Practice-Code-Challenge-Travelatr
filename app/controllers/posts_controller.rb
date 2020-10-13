c2cadc9fac9af61b9bbf9710db33d93bbd62e882
jrwatson94 <j.watsonreid@gmail.com>
2020-10-13 17:13:53 -0400

    nearly complete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def create
+        post = Post.create(post_params)
+        redirect_to post_path(post)
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(
+            :title,
+            :content,
+            :likes,
+            :blogger_id,
+            :destination_id)
+    end
+    
+end
